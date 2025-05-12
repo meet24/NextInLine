ee603dc1e2f06d73911f504dd28ab7d9c8cb3127
Meet Patel <meet241patel@gmail.com>
2025-05-12 14:48:17 -0400

    path changes

--- NIL-Frontend/src/api.js
+++ NIL-Frontend/src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://192.168.2.250:3001/api" });
 
-export const registerUser = (data) => API.post("/auth/register", data);
-export const loginUser = (data) => API.post("/auth/login", data);
+export const registerUser = (data) => API.post("/users/register", data);
+export const loginUser = (data) => API.post("/users/login", data);
