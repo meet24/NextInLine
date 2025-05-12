479dab45a8c0b3ff2c7cf3f44efdbedf9cbd3bd1
Meet Patel <meet241patel@gmail.com>
2025-05-12 16:01:05 -0400

    front-end URL setup

--- NIL-Frontend/src/api.js
+++ NIL-Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://192.168.2.250:3001/api" });
+const API = axios.create({ baseURL: `${process.env.EXPO_PUBLIC_FRONTEND_URL}:3001/api` });
 
 export const registerUser = (data) => API.post("/users/register", data);
 export const loginUser = (data) => API.post("/users/login", data);
